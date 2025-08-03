d9ac579902b3b666698c69e31e76e5694b630306
Taha ManaquibWala <taha.manaquib53@gmail.com>
2025-08-03 18:19:52 +0530

    puter auth setup

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/">
+        <p className="text-2xl font-bold text-gradient uppercase">resumind</p>
+      </Link>
+      <Link to="/upload" className="uppercase primary-button w-fit">
+        upload resume
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
